a8ffd14434d8350277e4b199f35314751a722d6c
luishenriquegianeti <169113501+luishenriquegianeti@users.noreply.github.com>
2025-09-18 18:31:53 +0000

    18/09/2025

---
+++ src/main/java/app/Compromisso.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.Calendar;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@setter
+@NoArgsConstructor
+public class Compromisso extends Atividade {
+    private Calendar dataInicial;
+    private Calendar dataFinal;
+
+    public Compromisso (String descricao, Calendar dataInicial, Calendar dataFinal){
+        this.setDescricao(descricao);
+        this.setDataInicial(dataInicial);
+        this.setDataFinal(dataFinal);
+    }
+
+    @Override
+    public String getDetalhes(){
+        return this.getDescricao() + "(" + this.getDataInicial().getTime() + ")";
+    }
+
+}
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Calendar;
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
         Aluno a1 = new Aluno();
@@ -10,5 +12,26 @@ public class PrimeiraClasse {
         System.out.println(a1.getIdade());
 
         System.out.println("Olá Gradle!!!");
+
+        Tarefa t1 = new Tarefa();
+        t1.setDescricao("Estudar Estruturas de Dados");
+        t1.setConcluido(true);
+
+        Tarefa t2 = t1;
+        t2.setConcluido(false);
+
+        System.out.println(t1.getDescricao());
+        System.out.println(t1.isConcluido());
+
+        Atividade[] objetos = new Atividade[3];
+
+        Calendar c = Calendar.getInstance();
+        c.set(2025, 8, 28);
+
+        objetos[0] = new Compromisso("Novo Compromisso",c ,c);
+        objetos[1] = new Tarefa("Nova Tarefa" , true);
+
+        System.out.println(objetos[0].getDetalhes());
+        System.out.println(objetos[1].getDetalhes());
     }
 }
